32bb63be957e1484f1865096afa4370a8c2b15df
Your Name <you@example.com>
2025-03-20 00:24:45 +0200

    Login form and pages

--- app.py
+++ app.py
@@ -1,9 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-# Helper function for BMR calculation
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'  # Where to redirect if not logged in
 
+# Dummy User Data (You should use a database in production)
+users = {
+    'Stoyan': {'password': 'Stoyan!'},
+    'Slaveya': {'password': 'Slaveya!'}
+}
+
+# User class for Flask-Login
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+# Load user function for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+# Login Form
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=20)])
+
+# Home page (redirect to login if not logged in)
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('main_page'))  # Redirect to the BMR calculator after login
+        else:
+            flash('Invalid username or password')
+
+    return render_template('login.html', form=form)
 
 def calculate_bmr(age, height, weight, gender):
     if gender == 'male':
@@ -11,7 +62,8 @@ def calculate_bmr(age, height, weight, gender):
     else:
         return 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
     
-@app.route('/')
+@app.route('/home')
+@login_required
 def main_page():
     return render_template('index.html')
 
@@ -55,6 +107,17 @@ def calculate_result():
 
     return render_template('bmr_page.html', result=result, error_message=error_message)
 
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('logged_out'))
+
+# New route for the logged out page
+@app.route('/logged_out')
+def logged_out():
+    return render_template('logged_out.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
